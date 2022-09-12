d7bfe448838f253b29c99e59840ed54d78016107
Chuqiao Li <chuqli@kringel.ee.ethz.ch>
2022-09-12 16:06:20 +0200

    add code

--- iCaRL/train_CNND.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH  --gres=gpu:1
-#SBATCH  --mem=60G
-source /scratch_net/kringel/chuqli/conda/etc/profile.d/conda.sh
-conda activate icarl_pytorch
-REPEAT=1
-############################################
-python main_icarl_CNND.py "$@"
--- iCaRL/train_CNND6.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH  --constraint='titan_xp|geforce_gtx_titan_x'
-#SBATCH  --gres=gpu:2
-#SBATCH  --mem=70G
-source /scratch_net/kringel/chuqli/conda/etc/profile.d/conda.sh
-conda activate icarl_pytorch
-############################################
-
-python main_icarl_CNND.py --name icarl_df_12_max05 --checkpoints_dir /srv/beegfs02/scratch/generative_modeling/data/Deepfake/Adam-NSCL/checkpoints --model_weights /home/chuqli/scratch/CNNDetection/checkpoints/no_aug/model_epoch_best.pth --dataroot /srv/beegfs02/scratch/generative_modeling/data/Deepfake/test --task_name gaugan,biggan,cyclegan,imle,deepfake,crn,wild,glow,stargan_gf,stylegan,whichfaceisreal,san --multiclass  0 0 1 0 0 0 0 1 1 1 0 0  --batch_size 32 --num_epochs 30 --schedule 10 20 30 --add_binary --binary_weight 0.5 --binary_loss max
